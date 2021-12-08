3a0cb7dfb874de8ea4e621aad03b6e7c4af29cb6
MH Motamedi <39155555+MHMotamedi97@users.noreply.github.com>
2021-12-08 15:58:29 +0330

    Update enwnew.py

--- enwnew.py
+++ enwnew.py
@@ -1,29 +1,5 @@
 import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import normalize
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.models import load_model
-from tensorflow.keras.optimizers import Adam
-import tensorflow.keras.backend as K
+
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-x_train = tf.convert_to_tensor(np.reshape(x_train, (x_train.shape[0], 1, x_train.shape[1])))
-x_test = tf.convert_to_tensor(np.reshape(x_test, (x_test.shape[0], 1, x_test.shape[1])))
-y_train = tf.convert_to_tensor(y_train.reshape((y_train.size, 1)))
-y_test = tf.convert_to_tensor(y_test.reshape((y_test.size, 1)))
 
-model = Sequential()
-model.add(LSTM(25, input_shape=(1, 10), return_sequences=True, activation = 'tanh', stateful=False))
-# model.add(LSTM(50, return_sequences=True, activation = 'tanh', stateful=False))
-# model.add(Dense(units=10, activation='relu'))
-
-# model.add(LSTM(50, return_sequences=True, activation = 'tanh', stateful=False))
-model.add(Dense(units=15, activation='relu'))
-model.add(Dense(units=1)) 
-model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=True))
-model.summary()
